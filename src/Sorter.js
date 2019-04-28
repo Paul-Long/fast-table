71c13cc470879ec8c503c5bac9f4c116aa4a6fe5
Paul <baolong.hou@sumscope.com>
2019-04-28 12:00:19 +0800

    1.4.8-beta.13

--- src/Sorter.js
+++ src/Sorter.js
@@ -18,6 +18,12 @@ function Sorter(props: Props) {
   if (order === true) {
     order = 'desc';
   }
+  let path =
+    'M556.8 876.8l134.4-134.4c25.6-25.6 25.6-64 0-89.6-25.6-25.6-64-25.6-89.6 0L576 678.4 576 192c0-38.4-25.6-64-64-64S448 153.6 448 192l0 486.4-25.6-25.6c-25.6-25.6-64-25.6-89.6 0-25.6 25.6-25.6 64 0 89.6l134.4 134.4C492.8 902.4 531.2 902.4 556.8 876.8z';
+  if (order === 'asc') {
+    path =
+      'M467.2 147.2 332.8 281.6c-25.6 25.6-25.6 64 0 89.6 25.6 25.6 64 25.6 89.6 0L448 345.6 448 832c0 38.4 25.6 64 64 64s64-25.6 64-64L576 345.6l25.6 25.6c25.6 25.6 64 25.6 89.6 0 25.6-25.6 25.6-64 0-89.6L556.8 147.2C531.2 121.6 492.8 121.6 467.2 147.2z';
+  }
   const onClick = () => {
     const o = order === Order.desc ? Order.asc : Order.desc;
     if (typeof onSort === 'function') {
@@ -25,10 +31,21 @@ function Sorter(props: Props) {
     }
   };
   const wrapperCls = `${prefixCls}-sorter`;
-  const sortClass = classNames(`${wrapperCls}-${order}`);
   return (
     <div className={wrapperCls} onClick={onClick}>
-      <div className={sortClass} />
+      <svg
+        viewBox='64 64 896 896'
+        className=''
+        data-icon='plus'
+        width='1em'
+        height='1em'
+        fill='currentColor'
+        aria-hidden='true'
+        focusable='false'
+        style={{display: 'inline-block'}}
+      >
+        <path d={path} />
+      </svg>
     </div>
   );
 }
